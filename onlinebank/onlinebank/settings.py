68071901b77354a2ac95ed7d193dd140475b6e9b
RFY <876320233@qq.com>
2024-06-05 20:50:26 +0800

    修改最大值输入问题

--- onlinebank/onlinebank/settings.py
+++ onlinebank/onlinebank/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'banking',
         'USER': 'root',
-        'PASSWORD': 'chenyihang2',
+        'PASSWORD': 'peopleflysheep0@',
         'HOST': 'localhost',
         'PORT': '3306',
     }
