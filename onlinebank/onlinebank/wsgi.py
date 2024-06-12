e979ed2554e23e6a94b08d49c075c4ee392e2a20
cabbagewyh <114276868+cabbagewyh@users.noreply.github.com>
2024-06-12 15:01:28 +0800

    部分权限控制+自动更新存款

--- onlinebank/onlinebank/wsgi.py
+++ onlinebank/onlinebank/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from counteropt.cashier_views import deposit_record_update
+from counteropt.cashier_views import time_deposit_record_update, demand_deposit_record_update
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinebank.settings')
 
 application = get_wsgi_application()
-deposit_record_update()
+time_deposit_record_update()
+demand_deposit_record_update()
